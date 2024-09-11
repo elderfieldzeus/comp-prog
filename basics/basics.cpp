7445723e6d274efd25849c6712aa9a34422dfe35
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-09-11 21:00:59 +0800

    feat: added very basic codes to remember header and syntax

---
+++ basics/basics.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h> // best for including all standard headers
+
+using namespace std; 
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ basics/input.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Your name is " << name;
+    return 0;
+}
